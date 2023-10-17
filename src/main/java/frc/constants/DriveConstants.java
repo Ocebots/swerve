aa191d94038461eee1ffa636d8a36b8cfd35aecc
BlueZeeKing <github@blueish.dev>
2023-10-17 17:49:35 -0400

    Fix formatting

--- src/main/java/frc/constants/DriveConstants.java
+++ src/main/java/frc/constants/DriveConstants.java
@@ -5,35 +5,37 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.util.Units;
 
 /**
- * Driving Parameters - Note that the maximums listed are not the maximum capable speeds of the robot, rather the allowed maximum speeds. All measurements will be in meters.
+ * Driving Parameters - Note that the maximums listed are not the maximum capable speeds of the
+ * robot, rather the allowed maximum speeds. All measurements will be in meters.
  */
 public class DriveConstants {
-    // Driving Parameters - Note that these are not the maximum capable speeds of
-    // the robot, rather the allowed maximum speeds
-    public static final double MAX_SPEED_METERS_PER_SECOND = 4.8;
-    public static final double MAX_ANGULAR_SPEED = 2 * Math.PI; // radians per second
+  // Driving Parameters - Note that these are not the maximum capable speeds of
+  // the robot, rather the allowed maximum speeds
+  public static final double MAX_SPEED_METERS_PER_SECOND = 4.8;
+  public static final double MAX_ANGULAR_SPEED = 2 * Math.PI; // radians per second
 
-    public static final double DIRECTION_SLEW_RATE = 1.2; // radians per second
-    public static final double MAGNITUDE_SLEW_RATE = 1.8; // percent per second (1 = 100%)
-    public static final double ROTATIONAL_SLEW_RATE = 2.0; // percent per second (1 = 100%)
+  public static final double DIRECTION_SLEW_RATE = 1.2; // radians per second
+  public static final double MAGNITUDE_SLEW_RATE = 1.8; // percent per second (1 = 100%)
+  public static final double ROTATIONAL_SLEW_RATE = 2.0; // percent per second (1 = 100%)
 
-    // Chassis configuration
-    public static final double TRACK_WIDTH_METERS = Units.inchesToMeters(26.5);
-    // Distance between centers of right and left wheels on robot
-    public static final double WHEEL_BASE_METERS = Units.inchesToMeters(26.5);
-    // Distance between front and back wheels on robot
-    public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
-            new Translation2d(WHEEL_BASE_METERS / 2, TRACK_WIDTH_METERS / 2),
-            new Translation2d(WHEEL_BASE_METERS / 2, -TRACK_WIDTH_METERS / 2),
-            new Translation2d(-WHEEL_BASE_METERS / 2, TRACK_WIDTH_METERS / 2),
-            new Translation2d(-WHEEL_BASE_METERS / 2, -TRACK_WIDTH_METERS / 2));
+  // Chassis configuration
+  public static final double TRACK_WIDTH_METERS = Units.inchesToMeters(26.5);
+  // Distance between centers of right and left wheels on robot
+  public static final double WHEEL_BASE_METERS = Units.inchesToMeters(26.5);
+  // Distance between front and back wheels on robot
+  public static final SwerveDriveKinematics DRIVE_KINEMATICS =
+      new SwerveDriveKinematics(
+          new Translation2d(WHEEL_BASE_METERS / 2, TRACK_WIDTH_METERS / 2),
+          new Translation2d(WHEEL_BASE_METERS / 2, -TRACK_WIDTH_METERS / 2),
+          new Translation2d(-WHEEL_BASE_METERS / 2, TRACK_WIDTH_METERS / 2),
+          new Translation2d(-WHEEL_BASE_METERS / 2, -TRACK_WIDTH_METERS / 2));
 
-    // Angular offsets of the modules relative to the chassis in radians
-    public static final double FRONT_LEFT_CHASSIS_ANGULAR_OFFSET = -Math.PI / 2;
-    public static final double FRONT_RIGHT_CHASSIS_ANGULAR_OFFSET = 0;
-    public static final double BACK_LEFT_CHASSIS_ANGULAR_OFFSET = Math.PI;
-    public static final double BACK_RIGHT_CHASSIS_ANGULAR_OFFSET = Math.PI / 2;
+  // Angular offsets of the modules relative to the chassis in radians
+  public static final double FRONT_LEFT_CHASSIS_ANGULAR_OFFSET = -Math.PI / 2;
+  public static final double FRONT_RIGHT_CHASSIS_ANGULAR_OFFSET = 0;
+  public static final double BACK_LEFT_CHASSIS_ANGULAR_OFFSET = Math.PI;
+  public static final double BACK_RIGHT_CHASSIS_ANGULAR_OFFSET = Math.PI / 2;
 
-    public static final boolean GYRO_IS_REVERSED = false;
-    public static final double FREE_SPEED_RPM = 5676;
+  public static final boolean GYRO_IS_REVERSED = false;
+  public static final double FREE_SPEED_RPM = 5676;
 }
